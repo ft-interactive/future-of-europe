0a6278a6a2adc540457aa80a3a376f1b9493131b
David Blood <davidbjourno@users.noreply.github.com>
2016-10-14 17:16:50 +0100

    Add JS functionality as proposed by @kavanagh

--- client/index.js
+++ client/index.js
@@ -1,12 +1,57 @@
-function randomPitch() {
-  // find all the pitches
-  // pick a random one// show it
-  // hide all the rest
+let currentIndex = {};
 
-  const random = Math.random() * 100 / 17
-  document.querySelectorAll('[data-index="'+ random +'"]')
+function showEntry(index) {
+  // Display div index
+  const div = document.querySelector(`div[data-index="${index}"]`);
+
+  div.style.display = 'block';
+
+  // Change the address bar
+  if (history.replaceState) {
+    history.replaceState(null, null, `#${index}`);
+  } else {
+    location.hash = `#${index}`;
+  }
 }
 
-myButton.onclick = randomPitch;
+function getRandomIndex(min, max) {
+  return Math.floor((Math.random() * (max - min)) + min);
+}
 
-randomPitch()
+function showRandom() {
+  const pitchCount = document.querySelector('div[data-pitch-count]').dataset.pitchCount;
+  let randomIndex = getRandomIndex(0, pitchCount);
+
+  // console.log(randomIndex);
+
+  if (randomIndex === currentIndex) {
+    // console.log('Duplicate index returned, re-randomising');
+    randomIndex = getRandomIndex(0, pitchCount - 1);
+    // console.log(randomIndex);
+  }
+
+  showEntry(randomIndex);
+
+  currentIndex = randomIndex;
+}
+
+const randomButton = document.querySelector('.random-button');
+
+randomButton.addEventListener('click', () => {
+  const previousDiv = document.querySelector(`div[data-index="${currentIndex}"]`);
+
+  showRandom();
+
+  previousDiv.style = '';
+});
+
+// On load
+const hashNumber = location.hash.slice(1);
+
+if (hashNumber && !Number.isNaN(hashNumber)) {
+  showEntry(hashNumber);
+
+  currentIndex = hashNumber;
+} else {
+  showRandom();
+}

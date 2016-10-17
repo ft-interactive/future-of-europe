4549d5f13a182974d5c32a2b99ad798145a047dc
David Blood <davidbjourno@users.noreply.github.com>
2016-10-17 16:19:27 +0100

    Give viewed pitches `.viewed` class and navigate to form page when all have been viewed

--- client/index.js
+++ client/index.js
@@ -1,10 +1,14 @@
+const pitchCount = parseInt(document.querySelector('div[data-pitch-count]').dataset.pitchCount, 10);
 let currentIndex = {};
+const randomButton = document.querySelector('.random-button');
+const hashNumber = location.hash.slice(1);
 
 function showEntry(index) {
   // Display div index
   const div = document.querySelector(`div[data-index="${index}"]`);
 
   div.style.display = 'block';
+  div.classList.add('viewed');
 
   // Change the address bar
   if (history.replaceState) {
@@ -19,35 +23,44 @@ function getRandomIndex(min, max) {
 }
 
 function showRandom() {
-  const pitchCount = document.querySelector('div[data-pitch-count]').dataset.pitchCount;
   let randomIndex = getRandomIndex(0, pitchCount);
+  const div = document.querySelector(`div[data-index="${randomIndex}"]`);
 
-  // console.log(randomIndex);
-
-  if (randomIndex === currentIndex) {
-    // console.log('Duplicate index returned, re-randomising');
+  if (div.classList.contains('viewed')) {
     randomIndex = getRandomIndex(0, pitchCount);
-    // console.log(randomIndex);
+
+    showRandom();
+  } else {
+    showEntry(randomIndex);
+
+    currentIndex = randomIndex;
   }
-
-  showEntry(randomIndex);
-
-  currentIndex = randomIndex;
 }
-
-const randomButton = document.querySelector('.random-button');
 
 randomButton.addEventListener('click', () => {
   const previousDiv = document.querySelector(`div[data-index="${currentIndex}"]`);
+  let viewedPitchCount = {};
+
+  showRandom();
 
   previousDiv.style = '';
 
-  showRandom();
+  viewedPitchCount = document.querySelectorAll('.viewed').length;
+
+  if (viewedPitchCount === pitchCount) {
+    randomButton.innerHTML = 'Submit Your Own';
+
+    randomButton.classList.add('submit-button');
+
+    randomButton.classList.remove('random-button');
+
+    randomButton.addEventListener('click', () => {
+      window.location.href = 'https://ig.ft.com/sites/future-of-britain-form/';
+    });
+  }
 });
 
 // On load
-const hashNumber = location.hash.slice(1);
-
 if (hashNumber && !Number.isNaN(hashNumber)) {
   showEntry(hashNumber);
 
